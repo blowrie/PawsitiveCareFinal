6d51a68adc16d9d51179c15ac7f237be7ac8c56e
Weiskri <kkweis13@gmail.com>
2018-04-11 13:31:35 -0400

    home page styling

---
+++ PawsitiveCare/Content/JavaScript.js
@@ -0,0 +1,8 @@
+﻿// Parallax
+if ('ontouchstart' in window == false) {
+    $('body').mousemove(function (e) {
+        var moveX = (e.pageX * -1 / 25);
+        var moveY = (e.pageY * -1 / 25);
+        $('.outer').css('background-position', moveX + 'px ' + moveY + 'px');
+    });
+}
