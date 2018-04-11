cc4e3660dbf665db1738ede78a7c19cd58f63585
Weiskri <kkweis13@gmail.com>
2018-04-11 18:53:49 -0400

    fixed images controller

--- PawsitiveCare/Controllers/ImagesController.cs
+++ PawsitiveCare/Controllers/ImagesController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 namespace PawsitiveCare.Controllers
@@ -12,6 +14,7 @@ namespace PawsitiveCare.Controllers
 
         public ActionResult Index()
         {
+            //var images = db.Images.Include(i => i.Pet);
             return View(db.Images.ToList());
         }
 
@@ -27,8 +30,8 @@ namespace PawsitiveCare.Controllers
             string fileName = Path.GetFileNameWithoutExtension(imageModel.ImageFile.FileName);
             string extension = Path.GetExtension(imageModel.ImageFile.FileName);
             fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
-            imageModel.ImagePath = "~/Image/" + fileName;
-            fileName = Path.Combine(Server.MapPath("~/Image"), fileName);
+            imageModel.ImagePath = "" + fileName;
+            fileName = Path.Combine(Server.MapPath("/Images"), fileName);
             imageModel.ImageFile.SaveAs(fileName);
             using (PCDbEntities db = new PCDbEntities())
             {
@@ -38,6 +41,114 @@ namespace PawsitiveCare.Controllers
 
             ModelState.Clear();
             return View();
+        }
+
+        // GET: Images1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Image image = db.Images.Find(id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(image);
+        }
+
+        // GET: Images1/Create
+        public ActionResult Create()
+        {
+            ViewBag.PetID = new SelectList(db.Pets, "PetID", "PetName");
+            return View();
+        }
+
+        // POST: Images1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ImageID,ImageTitle,ImagePath,PetID")] Image image)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Images.Add(image);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PetID = new SelectList(db.Pets, "PetID", "PetName", image.PetID);
+            return View(image);
+        }
+
+        // GET: Images1/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Image image = db.Images.Find(id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PetID = new SelectList(db.Pets, "PetID", "PetName", image.PetID);
+            return View(image);
+        }
+
+        // POST: Images1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ImageID,ImageTitle,ImagePath,PetID")] Image image)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(image).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PetID = new SelectList(db.Pets, "PetID", "PetName", image.PetID);
+            return View(image);
+        }
+
+        // GET: Images1/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Image image = db.Images.Find(id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(image);
+        }
+
+        // POST: Images1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Image image = db.Images.Find(id);
+            db.Images.Remove(image);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
