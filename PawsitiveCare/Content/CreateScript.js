e9e2c3e45084ef96148164262d4af79b9f8db17e
TAxford <t.axford2@yahoo.com>
2018-04-13 11:29:08 -0400

    Styled and lined up Create Page

--- PawsitiveCare/Content/CreateScript.js
+++ PawsitiveCare/Content/CreateScript.js
@@ -1,9 +1,1 @@
-﻿//function LogIn() {
-//    var LogIn = doc.GetElementByID.LogIn
-//    if ("@HttpContext.Current.User.Identity.GetUserId"){
-//        LogIn.Display.None;
-//    }
-//    else {
-//        LogIn.Display.None;
-//    }
-//}
+﻿
